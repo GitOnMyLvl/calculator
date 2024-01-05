77adde9ac8e492343a2914ba2f2f3eb25a6c1884
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-01-05 20:38:12 +0100

    Add clear and delet function, removed console.log and colorfull buttons

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,15 @@ const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number-button');
 const operatorButtons = document.querySelectorAll('.operator-button');
 const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
 let currentOperation = null;
 let inputNumber = 0;
 let storedNumber = 0;
 let result = 0;
 
 
-
+// Funnction to perform the operation
 function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
@@ -25,6 +27,18 @@ function operate(operator, num1, num2) {
 
 }
 
+const clearDisplay = () =>  display.textContent = 0;
+
+
+// Function to clear the variables of the calculator
+const clearCalc = () => {
+    currentOperation = null;
+    inputNumber = 0;
+    storedNumber = 0;
+    result = 0;
+};
+
+//Event listeners for each number button
 numberButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         if (display.textContent == 0) {
@@ -32,23 +46,21 @@ numberButtons.forEach((button) => {
         }
         display.textContent += e.target.textContent;
         inputNumber = Number(display.textContent);
-        console.log(inputNumber);
     });
 });
 
+// Event listeners for each operator button
 operatorButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         if (currentOperation !== null) {
-            console.log('current operation is not null');
             storedNumber = operate(currentOperation, storedNumber, inputNumber);
             currentOperation = e.target.textContent;
-            console.log(storedNumber);
         }else {
             currentOperation = e.target.textContent;
             storedNumber = inputNumber;
         }
         inputNumber = 0;
-        display.textContent = inputNumber;
+        clearDisplay();
     });
 });
 
@@ -57,8 +69,20 @@ equalsButton.addEventListener('click', () => {
         return;
     }
     result = operate(currentOperation, storedNumber, inputNumber);
-    console.log(result);
     display.textContent = result;
-    currentOperation = null;
-    inputNumber = 0;
+    clearCalc();
+});
+
+
+clearButton.addEventListener('click', () => {
+    clearDisplay();
+    clearCalc();
+});
+
+
+deleteButton.addEventListener('click', () => {
+    const currentInput = display.textContent;
+    display.textContent = currentInput.slice(0, -1);
+    inputNumber = Number(display.textContent);
+    display.textContent = inputNumber;
 });
